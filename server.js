dc6266b292d5bd1073385218eb2ca884a4a170f6
Eni-iyi~ <akinbuliabraham@gmail.com>
2023-04-20 22:50:14 +0100

    git hub

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+require("colors");
+const DbConnect = require("./config/Db");
+
+const port = process.env.port || 4000;
+
+DbConnect()
+  .then(() => {
+    app.listen(port, () => {
+      console.log("Server is up and running and Db Connected".bgBlue);
+    });
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
